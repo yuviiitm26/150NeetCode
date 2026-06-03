9acb31ccb88e00467a2bcd4ec33ab4201712379d
yuviiitm26 <136081607+yuviiitm26@users.noreply.github.com>
2026-06-04 01:01:53 +0530

    day 2 two sum

---
+++ Day_2.py
@@ -0,0 +1,16 @@
+#2 Two Sum Easy
+
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+class Solution(object):
+  def twosum(self, nums, target):
+    n=lens(nums)
+    nummap = {}
+    for i in range(n):
+      complement = target - nums[i]
+      if complement in nummap:
+        return [i, nummap[complement]]
+      nummap[nums[i]] = i
+    return []
