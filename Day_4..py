b298671a2ce51f7b9ae4798666c4895e31a63bbb
yuviiitm26 <136081607+yuviiitm26@users.noreply.github.com>
2026-06-20 22:39:12 +0530

    day_4 group_anagrams problem

---
+++ Day_4..py
@@ -0,0 +1,24 @@
+"""Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Medium
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat". The strings "nat" and "tan" are anagrams as they can be rearranged to form each other. The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other."""
+def groupanagrams(self, strs):
+  anagram_map={}
+  for word in strs:
+    sorted_word = "".join(sorted(word))
+
+    if sorted_word not in anagram_map:
+      anagram_map[sorted_word] = []
+
+    anagram_map[sorted_word].append(word)
+
+  return list(anagram_map.values())
